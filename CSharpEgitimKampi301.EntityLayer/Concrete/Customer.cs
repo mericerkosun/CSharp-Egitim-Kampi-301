17a453832753715267053ce0ddaeaeec001bac77
meric erkosun <meric.erkosun@gmail.com>
2025-01-07 16:13:14 +0300

    N Katmanlı Mimari Entity Layer

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Customer
+    {
+        public int customerId { get; set; }
+        public string customerName { get; set; }
+        public string customerSurname { get; set; }
+        public string customerDistrict { get; set; }
+        public string customerCity { get; set; }
+    }
+}

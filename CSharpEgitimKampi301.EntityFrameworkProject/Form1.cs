c125cb0639e219c1553cbfa049adece599e3eb5d
meric erkosun <meric.erkosun@gmail.com>
2025-01-11 11:26:52 +0300

    Entity Framework Metotları ile Proje Uygulaması

---
+++ CSharpEgitimKampi301.EntityFrameworkProject/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EntityFrameworkProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        CSharpEgitimKampiEntityFrameworkTravelDBEntities db = new CSharpEgitimKampiEntityFrameworkTravelDBEntities();
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.Guide.ToList(); // Entity Framework, ilgili tablodaki verileri çeker.
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Guide guide = new Guide();
+            guide.GuideName = txtName.Text;
+            guide.GuideSurname = txtSurname.Text;
+            db.Guide.Add(guide);
+            db.SaveChanges(); // Değişiklikleri kaydeder.
+
+            MessageBox.Show("Rehber Başarıyla Eklendi!");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtID.Text);
+            var removeValue = db.Guide.Find(id);
+            db.Guide.Remove(removeValue);
+            db.SaveChanges();
+            MessageBox.Show("Rehber Başarıya Silindi!");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtID.Text);
+            var updateValue = db.Guide.Find(id);
+            updateValue.GuideName = txtName.Text;
+            updateValue.GuideSurname = txtSurname.Text;
+            db.SaveChanges();
+
+            MessageBox.Show("Rehber Başarıyla Güncellendi!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void btnGetByID_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtID.Text);
+            var values = db.Guide.Where(x => x.GuideId == id).ToList();
+            dataGridView1.DataSource = values;
+        }
+    }
+}
